3a97171ae2813b950453573f156a99b9aafccfb8
rbankston <ralph.l.bankston@gmail.com>
2017-12-06 10:40:43 -0500

    update init helper

--- cmd/init.go
+++ cmd/init.go
@@ -23,13 +23,9 @@ import (
 // initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Initialize plotter",
+	Long: `plotter init is how you get started. This command checks if a plotter directory exists and if not creates the directory and The name takes your current 
+	.kube/config file and moves it to .kube/plotter/<name> along with creating .kube/plotter/backup for the backup function.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("init called")
 	},
