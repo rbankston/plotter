7197b46b01ada69bdd3490756ba77d286dbc9dbe
rbankston <ralph.l.bankston@gmail.com>
2017-12-06 10:42:57 -0500

    update active

--- cmd/active.go
+++ cmd/active.go
@@ -23,13 +23,8 @@ import (
 // activeCmd represents the active command
 var activeCmd = &cobra.Command{
 	Use:   "active",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Shows active plotter file",
+	Long:  `plotter gives you the name of the active plotter file.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("active called")
 	},
